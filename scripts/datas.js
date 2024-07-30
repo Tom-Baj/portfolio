8733380bbbcb932aa3eb946587db133344e783b4
Tom-Baj <140359887+Tom-Baj@users.noreply.github.com>
2024-07-30 16:58:09 +0200

    script update

---
+++ scripts/datas.js
@@ -0,0 +1,19 @@
+export const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Compétences", path: "/skills" },
+  { name: "Projets", path: "/projects" },
+  { name: "Contact", path: "/contact" },
+];
+
+export const skillsList = [
+  "HTML",
+  "CSS",
+  "Javascript",
+  "React",
+  "NodeJs",
+  "Express",
+  "MongoDB",
+];
+
+export const projectsList = [];
