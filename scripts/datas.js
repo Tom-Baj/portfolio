a7f5a493b86604c034049c59403aca2fc553e4b6
Tom-Baj <140359887+Tom-Baj@users.noreply.github.com>
2024-07-30 17:51:16 +0200

    scripts update

--- scripts/datas.js
+++ scripts/datas.js
@@ -16,4 +16,41 @@ export const skillsList = [
   "MongoDB",
 ];
 
-export const projectsList = [];
+export const projectsList = [
+  {
+    id: "vieux-grimoire",
+    title: "Vieux Grimoire",
+    description: "Site de vente de livres anciens",
+    link: "https://vieux-grimoire.netlify.app/",
+  },
+  {
+    id: "portfolio",
+    title: "Portfolio",
+    description: "Site de présentation de mes projets",
+    link: "https://portfolio-romain.netlify.app/",
+  },
+  {
+    id: "todo",
+    title: "Todo List",
+    description: "Application de gestion de tâches",
+    link: "https://todo-romain.netlify.app/",
+  },
+  {
+    id: "weather",
+    title: "Weather App",
+    description: "Application météo",
+    link: "https://weather-romain.netlify.app/",
+  },
+  {
+    id: "blog",
+    title: "Blog",
+    description: "Site de partage d'articles",
+    link: "https://blog-romain.netlify.app/",
+  },
+  {
+    id: "chat",
+    title: "Chat",
+    description: "Application de messagerie instantanée",
+    link: "https://chat-romain.netlify.app/",
+  },
+];
