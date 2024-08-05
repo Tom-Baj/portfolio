4ccb35b0947f1a76354788d0c1e4da542082871e
Tom-Baj <bajouxtom@gmail.com>
2024-08-05 20:55:02 +0200

    background update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 import { navLinks, skillsList, projectsList } from "./datas.js";
+import { rellax } from "./animations.js";
 
 const skillsListContainer = document.getElementById("skills-list");
 const navLinksContainer = document.getElementById("nav-list");
@@ -8,13 +9,29 @@ document.addEventListener("DOMContentLoaded", () => {
   NavLink(navLinks);
   SkillList(skillsList);
   ProjectsList(projectsList);
+  TypeWriting();
+  setupScrollAnimation();
 });
 
+/* Scroll Animation */
+function setupScrollAnimation() {
+  const image = document.querySelector(".home__image-container--image");
+
+  window.addEventListener("scroll", () => {
+    const scrollPosition = window.scrollY;
+    const maxScroll = document.body.scrollHeight - window.innerHeight;
+    const rotation = (scrollPosition / maxScroll) * 100;
+    image.style.transform = `rotate(${rotation}deg)`;
+  });
+}
+
+/* Nav Links */
 function NavLink(links) {
   links.forEach((link) => {
     const li = document.createElement("li");
-    li.classList.add("header__nav-list__item");
+    li.classList.add("header__container__nav-item");
     const a = document.createElement("a");
+    a.classList.add("header__container__nav-link");
     a.href = link.path;
     a.textContent = link.name;
     li.appendChild(a);
@@ -22,6 +39,7 @@ function NavLink(links) {
   });
 }
 
+/* Skills */
 function SkillList(skill) {
   for (let i = 0; i < skillsList.length; i++) {
     const element = skillsList[i];
@@ -31,6 +49,7 @@ function SkillList(skill) {
   }
 }
 
+/* Projects */
 function ProjectsList(project) {
   for (let i = 0; i < projectsList.length; i++) {
     const project = projectsList[i];
@@ -43,4 +62,23 @@ function ProjectsList(project) {
     `;
     projectsListContainer.appendChild(div);
   }
+}
+
+/* Type Writing */
+function TypeWriting() {
+  let typeWriting = document.querySelector(".type-writing");
+  let content = typeWriting.textContent;
+  typeWriting.textContent = "";
+
+  let i = 0;
+  let timer = setInterval(() => {
+    if (i < content.length) {
+      typeWriting.textContent += content.charAt(i);
+      i++;
+    } else {
+      clearInterval(timer);
+      typeWriting.classList.remove("type-writing");
+      typeWriting.classList.add("slow-cursor");
+    }
+  }, 150);
 }
