bf257e0e145ba7ddea35209ed71e999d971ad307
Tom-Baj <bajouxtom@gmail.com>
2024-08-09 03:10:55 +0200

    icons update

--- scripts/datas.js
+++ scripts/datas.js
@@ -1,19 +1,105 @@
 export const navLinks = [
   { name: "Home", path: "#presentation" },
   { name: "About", path: "#about" },
-  { name: "Compétences", path: "#skills" },
+  { name: "Skills", path: "#skills" },
   { name: "Projets", path: "#projects" },
   { name: "Contact", path: "#contact" },
 ];
 
 export const skillsList = [
-  "HTML",
-  "CSS / Sass",
-  "Javascript",
-  "React",
-  "NodeJs",
-  "Express",
-  "MongoDB",
+  {
+    name: "HTML",
+    icon: "html5",
+    progression: 0.9,
+  },
+  {
+    name: "CSS",
+    icon: "css3",
+    progression: 0.8,
+  },
+  {
+    name: "Sass",
+    icon: "sass",
+    progression: 0.8,
+  },
+  {
+    name: "Javascript",
+    icon: "javascript",
+    progression: 0.7,
+  },
+  {
+    name: "React",
+    icon: "react",
+    progression: 0.7,
+  },
+  {
+    name: "NodeJs",
+    icon: "nodejs",
+    progression: 0.6,
+  },
+  {
+    name: "Express",
+    icon: "express",
+    progression: 0.6,
+  },
+  {
+    name: "MongoDB",
+    icon: "mongodb",
+    progression: 0.6,
+  },
+];
+
+export const osList = [
+  {
+    name: "Windows",
+    icon: "windows-11",
+  },
+  {
+    name: "Linux",
+    icon: "linux",
+  },
+  {
+    name: "MacOS",
+    icon: "apple",
+  },
+];
+
+export const softwareList = [
+  {
+    name: "Visual Studio Code",
+    icon: "vscode",
+  },
+  {
+    name: "Figma",
+    icon: "figma",
+  },
+  {
+    name: "Photoshop",
+    icon: "photoshop",
+  },
+  {
+    name: "Illustrator",
+    icon: "illustrator",
+  },
+];
+
+export const utilsList = [
+  {
+    name: "Git",
+    icon: "git",
+  },
+  {
+    name: "Npm",
+    icon: "npm",
+  },
+  {
+    name: "Terminal",
+    icon: "terminal",
+  },
+  {
+    name: "Postman",
+    icon: "postman",
+  },
 ];
 
 export const projectsList = [
