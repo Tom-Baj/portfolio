8b62a64af735bcd8cb086e0d7f69d1a1b5ad1fbf
Tom-Baj <bajouxtom@gmail.com>
2024-08-13 02:31:14 +0200

    projets update

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,4 +2,15 @@ function rellax() {
   var rellax = new Rellax(".rellax");
 }
 
-export { rellax };
+function headerScrolled() {
+  const header = document.querySelector("header");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
+}
+
+export { headerScrolled, rellax };
